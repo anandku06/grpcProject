edc6df7cd8b308f8bc699dfc86ca993167a3860f
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-08-27 01:06:11 +0530

    implement InitMango function for MongoDB connection setup

--- db/db.go
+++ db/db.go
@@ -1,8 +1,34 @@
 package db
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 // go.mongodb.org/mongo-driver/mongo
 // go.mongodb.org/mongo-driver/mongo/options
 
 var UserCollections *mongo.Collection
+
+func InitMango() error {
+	// WithTimeout returns WithDeadline(parent, time.Now().Add(timeout)).
+	// Canceling this context releases resources associated with it
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) // if connection not happens, then after 10 seconds
+	defer cancel() // this is called if connection does'nt happen
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOptions)
+
+	if err != nil {
+		return err
+	}
+
+	UserCollections = client.Database("grpc").Collection("users")
+	// Collection gets a handle for a collection with the given name configured with the given CollectionOptions.
+	// Database returns a handle for a database with the given name configured with the given DatabaseOptions.
+
+	return nil
+}
