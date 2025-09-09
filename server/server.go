cd9ad1da6f8c84ec274ad9174fee1ff071f717dd
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-09 23:54:19 +0530

    implement UpdateUser and DeleteUser methods in UserServer, basic gRPC CRUD app is done!

--- server/server.go
+++ server/server.go
@@ -50,3 +50,35 @@ func (s *UserServer) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.U
 		Email:  user.Email,
 	}, nil
 }
+
+func (s *UserServer) UpdateUser(ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	filter := bson.M{"user_id": req.UserId}
+	update := bson.M{"$set": bson.M{
+		"name":     req.Name,
+		"age":      req.Age,
+		"email":    req.Email,
+		"password": req.Password,
+	}}
+
+	_, err := db.UserCollections.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.UserResponse{
+		UserId: req.UserId,
+		Name:   req.Name,
+		Age:    req.Age,
+		Email:  req.Email,
+	}, nil
+}
+
+func (s *UserServer) DeleteUser(ctx context.Context, req *pb.GetUserRequest) (*pb.DeleteResponse, error) {
+	_, err := db.UserCollections.DeleteOne(ctx, bson.M{"user_id": req.UserId})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.DeleteResponse{
+		Mssg: "User deleted successfully",
+	}, nil
+}
