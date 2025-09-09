814c863783727ba5dd51925081f6cab69a0bef46
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-09-09 23:26:45 +0530

    implement GetUser method to retrieve user details from MongoDB

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/anandku06/grpcProject/db"
 	pb "github.com/anandku06/grpcProject/grpc_mongo_crud/proto"
 	"github.com/anandku06/grpcProject/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 type UserServer struct {
@@ -23,6 +24,21 @@ func (s *UserServer) CreateUser(ctx context.Context, req *pb.UserRequest) (*pb.U
 	}
 
 	_, err := db.UserCollections.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.UserResponse{
+		UserId: user.UserId,
+		Name:   user.Name,
+		Age:    user.Age,
+		Email:  user.Email,
+	}, nil
+}
+
+func (s *UserServer) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.UserResponse, error) {
+	user := models.User{}
+	err := db.UserCollections.FindOne(ctx, bson.M{"user_id": req.UserId}).Decode(&user)
 	if err != nil {
 		return nil, err
 	}
